3aba82845ee0ed363b134a7eda1d88fad29a26f4
adil <imrani.adil@gmail.com>
2019-05-05 12:15:17 +0200

    iteration 1

---
+++ bank-account-kata/src/main/java/com/adil/bank_account_kata/App.java
@@ -0,0 +1,13 @@
+package com.adil.bank_account_kata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
