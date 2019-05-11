611e18359ae847e71411cc4edf8341c8210c53be
adil <imrani.adil@gmail.com>
2019-05-11 02:12:29 +0200

    iteration 11: refactoring and cleaning

--- bank-account-kata/src/main/java/com/bankaccount/exception/UnsuffcientBalanceException.java
+++ bank-account-kata/src/main/java/com/bankaccount/exception/UnsuffcientBalanceException.java
@@ -1,5 +1,6 @@
 package com.bankaccount.exception;
 
+//a good practice because RuntimeException does not have to be declared in a method's throws
 public class UnsuffcientBalanceException extends RuntimeException {
 	public UnsuffcientBalanceException(String message) {
 		super(message);
